83540f6b91c4ca763708c9d2bbfe11b98a50b515
web-dev <nathankim137311@gmail.com>
2021-09-25 00:35:26 -0700

    added eventlisteners to all inputs, and added displayBook function

--- app/js/script.js
+++ app/js/script.js
@@ -1,12 +1,43 @@
-console.log('hello everybody');
+// variables
+let bookTitle = document.getElementById('title'); 
+let authorName = document.getElementById('authorname');
+let numPages = document.getElementById('pages');
+let readit = document.getElementById('read'); 
+let addBtn = document.getElementById('add-btn'); 
+let myLibrary = [];
+let title = null; 
+let author = null;
+let pages = null;
+let read = null;  
+let book = new Book(title, author, pages, read);
 
-let addNewBookBtn = document.getElementById('new-book'); 
-let myLibrary = []; 
-let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '296 pages', 'not read yet'); 
+// let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '296 pages', 'not read yet'); 
 
-addNewBookBtn.addEventListener('click', () => {
-  console.log('button pressed');
+// event listeners 
+bookTitle.addEventListener('input', () => {
+  title = bookTitle.value;
+});
+ 
+authorName.addEventListener('input', () => {
+  author = authorName.value;
+});
+
+numPages.addEventListener('input', () => {
+  pages = numPages.value;
+});
+
+readit.addEventListener('input', () => {
+  read = readit.value;
 }); 
+
+
+addBtn.addEventListener('click', () => {
+  book = new Book(title, author, pages, read); 
+  addBookToLibrary();
+  displayBook()
+  console.log(book.info()); 
+  console.log(myLibrary);
+});
 
 // book constructor
 function Book(title, author, pages, read) {
@@ -20,5 +51,6 @@ function Book(title, author, pages, read) {
 }
 
 function addBookToLibrary() {
-  // do stuff here 
+    myLibrary.push(book); 
 }
+
