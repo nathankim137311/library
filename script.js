c7996ece92a28d5a69d5ed251b20dd57b919646a
web-dev <nathankim137311@gmail.com>
2021-09-24 19:47:24 -0700

    added object constructor function

---
+++ script.js
@@ -0,0 +1,14 @@
+// object constructor 
+function Book(title, author, pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+  this.info = function() {
+    return title + ' ' + 'by' + ' ' + author + ',' + ' ' + pages + ',' + ' ' + read; 
+  }
+}
+
+let theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '296 pages', 'not read yet'); 
+
+console.log(theHobbit.info()); 
