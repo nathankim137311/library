a52b9d39d77b891fe2b9902a4b3afa5efede1914
Kim <nathanjkim@student.cascadia.edu>
2021-10-05 17:40:12 -0700

    refactored createBook function and added getBook function, adding new entries to list is functional

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,28 @@
-// variables
+// DOM elements 
 const body = document.body;
+// list container 
+const containerDiv = document.createElement('div');
+body.appendChild(containerDiv); 
+containerDiv.classList.add('book-container');  
+// create list
+const ul = document.createElement('ul');
+ul.setAttribute('id', 'book-list');
+containerDiv.appendChild(ul);  
+const bookList = document.getElementById('book-list'); 
+// buttons 
+const clearStorageBtn = document.getElementById('clear-storage'); 
+const addBtn = document.getElementById('add-btn'); 
+const resetBtn = document.getElementById('reset')
 // input variables
 const titleInput = document.getElementById('title'); 
 const authorInput = document.getElementById('author');
 const pagesInput = document.getElementById('pages');
 const statusInput = document.getElementById('status'); 
-const addBtn = document.getElementById('add-btn'); 
 // arrays 
 const inputs = Array.from(document.querySelectorAll('input')); 
 let myLibrary = [];
+// iterator 
+let index = 0;
 
 // creates multiple book objects 
 class Book {
@@ -19,9 +33,13 @@ class Book {
     this.status = statusInput.value;
   }
   // prototype
-  getInfo() {
-    return `${this.title}(${this.pages}) was written by ${this.author}`
-  }
+  readStatus () {
+    if (this.status === 'read') {
+      return true; 
+    } else if (this.status === 'not read') {
+      return false; 
+    }
+  } 
 } 
 
 // clears input fields 
@@ -29,140 +47,68 @@ function clearInputs() {
   for(let i = 0; i < inputs.length; i++) {
     inputs[i].value = ''; 
   }
+  resetBtn.value = 'Reset'; 
 }
 
-
-
-
+// prevents form from submitting, calls functions
 addBtn.addEventListener('click', (e) => {
   e.preventDefault(); 
-  const book = new Book(); 
-  myLibrary.push(book); 
-  console.log(myLibrary); 
-  console.log(book); 
+  getBooks();
   clearInputs(); 
 });
 
-
-// listens for keyboard inputs and stores text data in variables
-
-/*
-// clear storage link
-const clearStorage = document.getElementById('clear-storage'); 
-clearStorage.addEventListener('click', () => {
-  localStorage.clear(); 
-});
-
-// list container 
-const containerDiv = document.createElement('div');
-containerDiv.classList.add('book-container');  
-body.appendChild(containerDiv); 
-
-// create list
-const ul = document.createElement('ul');
-ul.setAttribute('id', 'book-list');
-containerDiv.appendChild(ul);  
-const bookList = document.getElementById('book-list'); 
-
-// increment 
-let i = 0; 
-
-// event listeners
-bookTitle.addEventListener('input', () => {
-  title = bookTitle.value;
-});
- 
-authorName.addEventListener('input', () => {
-  author = authorName.value;
-});
-
-numPages.addEventListener('input', () => {
-  pages = numPages.value;
-});
-
-readit.addEventListener('input', () => {
-  read = readit.value;
-}); 
-
-addBtn.addEventListener('click', (e) => {
-  if (title === '' && author === '' && pages === '') {
-    e.preventDefault(); 
-    alert('Please fill out all input fields!'); 
-  } else {
-    e.preventDefault(); 
-    book = new Book(title, author, pages, read); 
-    myLibrary.push(book); 
-    createBook(myLibrary[i]);
-    i++;  
-  }
-});
-
-// get books from local storage
-if (localStorage.getItem('books') === null) {
-  myLibrary = [];
-} else {
-  const booksFromStorage = JSON.parse(localStorage.getItem('books'));
-  myLibrary = booksFromStorage; 
-  let book = 0; 
-  i = myLibrary.length; 
-  while (book < myLibrary.length) {
-    createBook(myLibrary[book]);
-    book++;
-  }
+// creates new book object and stores it in myLibrary
+function getBooks() {
+  const book = new Book(); 
+  myLibrary.push(book); 
+  createBook(myLibrary[index]);
+  index++;
 }
 
-// book constructor
-function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
-}
-
-function createBook (item) {
+function createBook (book) {
   saveLocal(); 
   // new book entry
   const li = document.createElement('li');
   const bookDiv = document.createElement('div'); 
-  bookDiv.setAttribute('id', myLibrary.indexOf(item)); 
+  bookDiv.setAttribute('id', myLibrary.indexOf(book)); 
   bookDiv.classList.add('book-div');
   li.appendChild(bookDiv);
   bookList.appendChild(li);
   // title
   const titleP = document.createElement('p');
-  titleP.textContent = ' ' + item.title; 
+  titleP.textContent = ' ' + book.title; 
   bookDiv.appendChild(titleP);
   // author 
   const authorP = document.createElement('p');
-  authorP.textContent = '' + item.author;    
+  authorP.textContent = ' ' + book.author;    
   bookDiv.appendChild(authorP); 
   // page
   const pageP = document.createElement('p');
-  pageP.textContent = item.pages + ' ' + 'pages';
+  pageP.textContent = book.pages + ' ' + 'pages';
   bookDiv.appendChild(pageP);
   // read button
   const readButton = document.createElement('button');
   readButton.setAttribute('id', 'read-button');
-  readButton.textContent = item.read + "";
+  readButton.textContent = book.status + "";
   bookDiv.appendChild(readButton);
-  if (item.read === 'read') {
+  if (book.readStatus() === true) {
     readButton.style.backgroundColor = 'green'; 
-  } else if (item.read === 'not read') {
+  } else if (book.readStatus() === false) {
     readButton.style.backgroundColor = '#b80202';
   } else {
     readButton.style.backgroundColor = 'white'; 
     readButton.style.color = 'black'; 
   }
-  readButton.addEventListener('click', (e) => {
+  readButton.addEventListener('click', () => {
     if (readButton.textContent === 'read') {
       readButton.style.backgroundColor = '#b80202';
       readButton.textContent = 'not read';
-      item.read = 'not read';
+      book.status = 'not read';
       saveLocal(); 
     } else if (readButton.textContent === 'not read') {
       readButton.style.backgroundColor = 'green'; 
       readButton.textContent = 'read';
-      item.read = 'read';
+      book.status = 'read';
       saveLocal(); 
     }
   });
@@ -171,7 +117,7 @@ function createBook (item) {
   deleteButton.setAttribute('id', 'delete-button'); 
   deleteButton.textContent = 'Delete'; 
   bookDiv.appendChild(deleteButton);
-  deleteButton.addEventListener('click', (e) => {
+  deleteButton.addEventListener('click', (e) => { // fix
     title = '';
     author = '';
     pages = '';
@@ -191,9 +137,29 @@ function createBook (item) {
   resetBtn.value = 'Reset'; 
 }
 
+
 // save to local storage 
 function saveLocal () {
   localStorage.setItem('books', JSON.stringify(myLibrary));
 }
-*/
+
+// get books from local storage
+if (localStorage.getItem('books') === null) {
+  myLibrary = [];
+} else {
+  const booksFromStorage = JSON.parse(localStorage.getItem('books'));
+  myLibrary = booksFromStorage; 
+  let index = 0; 
+  i = myLibrary.length; 
+  while (index < myLibrary.length) {
+    createBook(myLibrary[index]);
+    index++;
+  }
+}
+
+// clears local storage
+clearStorageBtn.addEventListener('click', () => {
+  localStorage.clear(); 
+});
+
 
